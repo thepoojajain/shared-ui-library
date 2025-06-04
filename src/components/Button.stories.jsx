37b32301a393b9b67a82892bdfaa53f8ec766cf3
PoojaJainACS <pooja.jain@acs.org.au>
2025-06-04 11:37:27 +1000

    setup: storybook

---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,10 @@
+// src/components/Button.stories.tsx
+import React from 'react';
+import  Button  from './Button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+};
+
+export const Primary = () => <Button>Click me</Button>;
